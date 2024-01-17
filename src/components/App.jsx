eb8a5d9b3b15fbe73f3769f05b867aa60988146f
Serhii-Fesianov <littlefox06042000@gmail.com>
2024-01-17 16:47:19 +0200

    update class component on functional with hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,56 +1,54 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Section } from './Section/Section';
 import { FeedbackOptions } from 'components/FeedbackOptions/FeedbackOptions';
 import { Statistics } from 'components/Statistics/Statistics';
 
-export class App extends Component {
-  state = {
+export const App = () => {
+  const [app, setApp] = useState({
     good: 0,
     neutral: 0,
     bad: 0,
+  });
+
+  const handleFeedback = option => {
+    setApp(prevState => ({ ...prevState, [option]: prevState[option] + 1 }));
   };
 
-  handleFeedback = option => {
-    this.setState(prevState => ({ [option]: prevState[option] + 1 }));
+  const countTotalFeedback = () => {
+    return app.good + app.neutral + app.bad;
   };
 
-  countTotalFeedback = () => {
-    return this.state.good + this.state.neutral + this.state.bad;
-  };
-
-  countPositiveFeedbackPercentage = () => {
-    const total = this.countTotalFeedback();
-    const { good } = this.state;
+  const countPositiveFeedbackPercentage = () => {
+    const total = countTotalFeedback();
+    const { good } = app;
     return Math.round((good * 100) / total);
   };
 
-  render() {
-    const total = this.countTotalFeedback();
-    const positiveFeedback = this.countPositiveFeedbackPercentage();
-    const { good, neutral, bad } = this.state;
-    return (
-      <>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={Object.keys(this.state)}
-            handleFeedback={this.handleFeedback}
+  const total = countTotalFeedback();
+  const positiveFeedback = countPositiveFeedbackPercentage();
+  const { good, neutral, bad } = app;
+  return (
+    <>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={Object.keys(app)}
+          handleFeedback={handleFeedback}
+        />
+      </Section>
+      <Section>
+        <h2>Statistics</h2>
+        {total > 0 ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positiveFeedback={positiveFeedback}
           />
-        </Section>
-        <Section>
-          <h2>Statistics</h2>
-          {total > 0 ? (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={total}
-              positiveFeedback={positiveFeedback}
-            />
-          ) : (
-            <p>No feedback</p>
-          )}
-        </Section>
-      </>
-    );
-  }
-}
+        ) : (
+          <p>No feedback</p>
+        )}
+      </Section>
+    </>
+  );
+};
